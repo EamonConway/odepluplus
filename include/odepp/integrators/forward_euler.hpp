a237e6972815ed8d350f4fd4a4199d7a82f72efa
Eamon Conway <conway.e@wehi.edu.au>
2023-08-14 20:15:51 +1000

    Change symbol for Args.
    Args has been changed to FnArgs.
    This is more explicit in what the arguments are used for.

--- include/odepp/integrators/forward_euler.hpp
+++ include/odepp/integrators/forward_euler.hpp
@@ -5,11 +5,11 @@ namespace odepp {
 namespace integrator {
 struct forward_euler_fn {
  public:
-  template <class State, class Fn, class... Args>
+  template <class State, class Fn, class... FnArgs>
   State operator()(Fn&& f, const RealType dt, RealType& t, const State& y0,
-                   Args&&... args) const {
+                   FnArgs&&... args) const {
     auto y = y0;
-    auto fn = f(t, y, std::forward<Args>(args)...);
+    auto fn = f(t, y, std::forward<FnArgs>(args)...);
 
     if constexpr (std::is_floating_point_v<State>) {
       y += dt * fn;
--- include/odepp/ode_solve.hpp
+++ include/odepp/ode_solve.hpp
@@ -6,20 +6,21 @@
 #include "odepp/types.hpp"
 namespace odepp {
 
-template <class Integrator, class Fn, class State, class... Args>
-  requires std::is_invocable_r_v<State, Fn, RealType&, const State&, Args&&...>
+template <class Integrator, class Fn, class State, class... FnArgs>
+  requires std::is_invocable_r_v<State, Fn, RealType&, const State&,
+                                 FnArgs&&...>
 auto ode_solve(Integrator&& integrate, Fn&& f, const RealType t0,
                const RealType t1, const RealType dt, const State y0,
-               Args&&... args) {
+               FnArgs&&... args) {
   using OutputType = std::pair<std::vector<RealType>, std::vector<State>>;
 
   auto t = t0;
   auto y = y0;
-  // Create the empty output
+  // Create output with the initial timestep.
   auto output = OutputType{{t}, {y}};
   while (t < t1) {
     // Do we want one_step to update t?
-    y = integrate(f, dt, t, y, std::forward<Args>(args)...);
+    y = integrate(f, dt, t, y, std::forward<FnArgs>(args)...);
     // We can add a check to determine if we want to log this timestep or not.
     output.first.emplace_back(t);
     output.second.emplace_back(y);
