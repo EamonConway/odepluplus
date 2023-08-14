0ac260e024a4559dad056c6b58f91fe7b2fb14b5
Eamon Conway <conway.e@wehi.edu.au>
2023-08-14 17:16:31 +1000

    Forward euler integrator type.
    This function runs a single step of the forward euler style integrator.

---
+++ include/odepp/integrators/forward_euler.hpp
@@ -0,0 +1,29 @@
+#ifndef ODEPP_INTEGRATORS_FE_HPP
+#define ODEPP_INTEGRATORS_FE_HPP
+#include "odepp/types.hpp"
+namespace odepp {
+namespace integrator {
+struct forward_euler_fn {
+ public:
+  template <class State, class Fn, class... Args>
+  State operator()(RealType& t, const RealType dt, const State& y0, Fn&& f,
+                   Args&&... args) const {
+    auto y = y0;
+    auto fn = f(t, y, std::forward<Args>(args)...);
+
+    if constexpr (std::is_floating_point_v<State>) {
+      y += dt * fn;
+    } else {
+      for (auto i = 0; i < fn.size(); ++i) {
+        y.at(i) += dt * fn[i];
+      }
+    }
+    t += dt;
+    return y;
+  };
+};
+
+inline constexpr forward_euler_fn forward_euler{};
+}  // namespace integrator
+}  // namespace odepp
+#endif
