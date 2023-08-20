22bdf8607e3a7ec4ea83c1feb8344a9d5df87c99
Eamon Conway <conway.e@wehi.edu.au>
2023-08-21 08:41:00 +1000

    Include special function for forward euler call.

---
+++ include/odepp/ode_forward_euler.hpp
@@ -0,0 +1,16 @@
+#ifndef ODEPP_ODE_FORWARD_EULER_HPP
+#define ODEPP_ODE_FORWARD_EULER_HPP
+
+#include "odepp/integrators/forward_euler.hpp"
+#include "odepp/ode_explicit_solve.hpp"
+
+namespace odepp {
+template <class State, typename Fn, class... FnArgs>
+constexpr auto ode_forward_euler(Fn&& f, const RealType t0, const RealType t1,
+                                 const RealType dt, const State& y0,
+                                 FnArgs&&... args) {
+  return ode_explicit_solve(integrator::forward_euler, f, t0, t1, dt, y0,
+                            std::forward<FnArgs>(args)...);
+}
+}  // namespace odepp
+#endif
