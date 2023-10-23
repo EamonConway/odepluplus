bd8fb50127e2d4182cf4081834e2e666d4e6efbc
Eamon Conway <conway.e@wehi.edu.au>
2023-10-23 14:42:41 +1100

    Define concepts for Implicit solutions.

--- include/odepp/ode_explicit_solve.hpp
+++ include/odepp/ode_explicit_solve.hpp
@@ -39,7 +39,6 @@ concept ExplicitIntegrator =
 /**
  * @brief
  *
- * @tparam Integrator
  * @tparam State
  * @tparam FnArgs
  * @tparam Fn
@@ -66,6 +65,70 @@ auto ode_solve(Integrator&& integrate, Fn&& f, const RealType t0,
     // Do we want one_step to update t?
     y = integrate(f, dt, t, y, std::forward<FnArgs>(args)...);
     // We can add a check to determine if we want to log this timestep or not.
+    output.first.emplace_back(t);
+    output.second.emplace_back(y);
+  }
+  return output;
+}
+
+/**
+ * @brief Implicit Ordinary differential equations must be invocable in the form
+ * of f(t,y,yp,arguments) and return a type that is equivalent to the type of y.
+ *
+ * @tparam Fn
+ * @tparam State
+ * @tparam FnArgs
+ */
+template <typename Fn, typename State, typename... FnArgs>
+concept ImplicitOdeFn =
+    std::is_invocable_r_v<State, Fn, const RealType, const State&, const State&,
+                          FnArgs&&...>;
+
+/**
+ * @brief Implicit Ordinary Differential Equations must be solved using an
+ * Implicit integrator.
+ *
+ * @tparam Fn
+ * @tparam State
+ * @tparam FnArgs
+ */
+template <typename Integrator, typename Fn, typename State, typename... FnArgs>
+concept ImplicitIntegrator =
+    std::is_invocable_r_v<State, Integrator, Fn, const RealType, RealType&,
+                          const State&, const State&, FnArgs&&...>;
+
+/**
+ * @brief
+ *
+ * @tparam State
+ * @tparam FnArgs
+ * @tparam Fn
+ * @tparam Integrator
+ * @param integrate
+ * @param f
+ * @param t0
+ * @param t1
+ * @param dt
+ * @param y0
+ * @param yp0
+ * @param args
+ * @return OdeOutput<State>
+ */
+template <class State, class... FnArgs, ImplicitOdeFn<State, FnArgs...> Fn,
+          ImplicitIntegrator<State, FnArgs...> Integrator>
+auto ode_solve(Integrator&& integrate, Fn&& f, const RealType t0,
+               const RealType t1, const RealType dt, const State& y0,
+               const State& yp0, FnArgs&&... args) -> OdeOutput<State> {
+  auto t = t0;
+  auto y = y0;
+  auto yp = yp0;
+  // Create output with the initial timestep.
+  auto output = OdeOutput<State>{{t}, {y}};
+  while (t < t1) {
+    // Do we want one_step to update t?
+    y = integrate(f, dt, t, y, yp, std::forward<FnArgs>(args)...);
+    // We can add a check to determine if we want to log this timestep or
+    // not .
     output.first.emplace_back(t);
     output.second.emplace_back(y);
   }
