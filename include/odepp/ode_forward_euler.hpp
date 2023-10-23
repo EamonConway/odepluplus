0f5ab8f95aa0727428a8cecfdb267e44a58cfb78
Eamon Conway <conway.e@wehi.edu.au>
2023-10-23 14:44:25 +1100

    Call ode_solve instead.

--- include/odepp/ode_forward_euler.hpp
+++ include/odepp/ode_forward_euler.hpp
@@ -2,15 +2,15 @@
 #define ODEPP_ODE_FORWARD_EULER_HPP
 
 #include "odepp/integrators/forward_euler.hpp"
-#include "odepp/ode_explicit_solve.hpp"
+#include "odepp/ode_solve.hpp"
 
 namespace odepp {
 template <class State, typename Fn, class... FnArgs>
 constexpr auto ode_forward_euler(Fn&& f, const RealType t0, const RealType t1,
                                  const RealType dt, const State& y0,
                                  FnArgs&&... args) {
-  return ode_explicit_solve(integrator::forward_euler, f, t0, t1, dt, y0,
-                            std::forward<FnArgs>(args)...);
+  return ode_solve(integrator::forward_euler, f, t0, t1, dt, y0,
+                   std::forward<FnArgs>(args)...);
 }
 }  // namespace odepp
 #endif
