c4ce369dbd80ead71236bc7b5ac07557a7e20f7d
Eamon Conway <conway.e@wehi.edu.au>
2024-01-18 11:21:00 +1100

    Warning on comparison of int and size_t fix

--- include/odepp/integrators/forward_euler.hpp
+++ include/odepp/integrators/forward_euler.hpp
@@ -7,21 +7,22 @@ namespace integrator {
 struct forward_euler_fn {
 public:
   template <class State, class Fn, class... FnArgs>
-  State operator()(Fn &&f, const RealType dt, RealType &t, const State &y0,
-                   FnArgs &&...args) const {
+  [[nodiscard("State must be used")]] State
+  operator()(Fn &&f, const RealType dt, RealType &t, const State &y0,
+             FnArgs &&...args) const {
     auto y = y0;
     auto fn = f(t, y, std::forward<FnArgs>(args)...);
 
     if constexpr (std::is_floating_point_v<State>) {
       y += dt * fn;
     } else {
-      for (auto i = 0; i < fn.size(); ++i) {
+      for (std::size_t i = 0; i < fn.size(); ++i) {
         y.at(i) += dt * fn[i];
       }
     }
     t += dt;
     return y;
-  };
+  }
 };
 
 inline constexpr forward_euler_fn forward_euler{};
